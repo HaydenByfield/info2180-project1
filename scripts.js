2224052821b8e518f2cc0931003a64a7812cc122
HaydenByfield <149196349+HaydenByfield@users.noreply.github.com>
2025-11-04 04:19:00 -0500

    Entire JavaScript and CSS changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+window.onload  = function(){
+    const form = document.querySelector('form');
+    const email = document.getElementById('email')
+    let message = document.querySelector('.message')
+
+    form.addEventListener('submit', (sub)=> {
+        sub.preventDefault();
+        let Messages = []
+        const emailValue = email.value;
+        if(emailValue === "" || emailValue.includes('@') === false || emailValue.includes('.') === false){
+            Messages.push("Please enter a valid email address.");   
+            message.style.color = 'red';
+        }else if(emailValue !== "" && emailValue.includes('@') && emailValue.includes('.')){
+            Messages.push( "Thank you! Your email address "  + emailValue + " has been added to our mailing list!")
+            message.style.color = 'blue';
+        }
+        message.innerText =  Messages.join("\n");
+          
+    })
+
+}
